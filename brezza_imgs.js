faf114488ff7c7d597e2c95c58405100879c976b
shubham-tam <105226707+shubham-tam@users.noreply.github.com>
2022-07-04 15:40:01 +0545

    Update brezza_imgs.js

--- brezza_imgs.js
+++ brezza_imgs.js
@@ -6,8 +6,8 @@ const filepath = path.resolve(__dirname, "images");
 
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
-  const url =
-    "https://hamrobazaar.com/cars/maruti-suzuki/suzuki-brezza-zdi-2016-in-nepal/3f16e3ed36d545418acddfadad3f33eb";
+  const url = "insert url"
+//     "https://hamrobazaar.com/cars/maruti-suzuki/suzuki-brezza-zdi-2016-in-nepal/3f16e3ed36d545418acddfadad3f33eb";
 
   const page = await browser.newPage();
   await page.goto(url, { waitUntil: "networkidle0" });
